801afd23602f12b8cc7d0db8e38b824d0e49e221
Aditya <63653252+Adia12528@users.noreply.github.com>
2022-04-12 19:20:24 +0530

    Add files via upload

---
+++ kali-nethunter_full_version_2022_without_root.py
@@ -0,0 +1,23 @@
+import os
+
+print("This module is for installing Kali linux without root in termux.")
+os.system("pkg update")
+os.system("pkg upgrade -y")
+print("It will take some time be patient.")
+os.system("pkg install fish -y")
+print("Enter into fish mode")
+os.system("fish")
+print("give permissions to phone storage 'press allow'")
+os.system("termux-setup-storage")
+os.system("pkg install wget -y")
+print("Initializing Kali nethunter repo")
+os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+os.system("chmod +x install-nethunter-termux")
+print("Downloading and installing kali linux in termux")
+os.system("./install-nethunter-termux")
+print("set password for VNC or start kex")
+os.system("nethunter kex passwd")
+print("Finally start VNC server.")
+os.system("nethunter kex &")
+os._exit(1)
+
